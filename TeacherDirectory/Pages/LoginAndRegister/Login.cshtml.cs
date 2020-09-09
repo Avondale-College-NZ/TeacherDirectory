3a6dd791ca8621ba5bca1f2594fe7d063eb72cdb
Shayen Kesha <ac98811@avcol.school.nz>
2020-09-09 15:11:58 +1200

    Creation of Login and Register Razor pages

---
+++ TeacherDirectory/Pages/LoginAndRegister/Login.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeacherDirectory.Pages.LoginAndRegister
+{
+    public class LoginModel : PageModel
+    {
+        [Required]
+        [StringLength(40, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+    
+    if(){}
+    else(){}
+}
