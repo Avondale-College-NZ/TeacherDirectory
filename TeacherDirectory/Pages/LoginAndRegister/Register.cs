6b13b66bbf005ca0d316cbc477fa3601382e5697
Shayen Kesha <ac98811@avcol.school.nz>
2020-09-14 11:09:23 +1200

    Register Page modifications

--- TeacherDirectory/Pages/LoginAndRegister/Register.cs
+++ TeacherDirectory/Pages/LoginAndRegister/Register.cs
@@ -11,6 +11,19 @@ namespace TeacherDirectory.Pages.LoginAndRegister
     public class IndexModel : PageModel
     {
         [Required]
+        [EmailAddressAttribute]
+        public string Email { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Password { get; set; }
+
+        [Required]
+        public string Department { get; set; }
     }
 }
